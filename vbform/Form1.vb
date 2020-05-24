9eea474b8a27898c4ba9d0bfa9f9a326defdbe47
ashvita0000 <ashvita0000@gmail.com>
2020-05-24 23:53:08 +0530

    added

---
+++ vbform/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub ProgressBar1_Click(sender As Object, e As EventArgs) Handles ProgressBar1.Click
+        ProgressBar1.ForeColor = Color.Black
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ProgressBar1.Visible = True
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ProgressBar1.Value = ProgressBar1.Value + 1
+        If ProgressBar1.Value >= 100 Then
+            Timer1.Stop()
+            MsgBox("Login Successful", MsgBoxStyle.Information)
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ProgressBar1.Visible = False
+    End Sub
+End Class
